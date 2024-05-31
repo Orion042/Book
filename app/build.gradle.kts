03aa2d82037b75f0010530718825efb5a80c7199
Orion042 <orion042.code@gmail.com>
2024-06-01 05:54:13 +0900

    gemini-proと会話できるように実装

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 
 dependencies {
 
-    implementation("com.google.ai.client.generativeai:generativeai:0.1.1")
+    implementation("com.google.ai.client.generativeai:generativeai:0.6.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/book/ui/gemini/GeminiFragment.kt
+++ app/src/main/java/com/example/book/ui/gemini/GeminiFragment.kt
@@ -1,24 +1,35 @@
 package com.example.book.ui.gemini
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.book.BuildConfig
 import com.example.book.Message
-import com.example.book.R
 import com.example.book.User
 import com.example.book.adapter.MessageListAdapter
-import com.example.book.databinding.FragmentAccountBinding
 import com.example.book.databinding.FragmentGeminiBinding
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.Content
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.time.LocalDateTime
 
 class GeminiFragment : Fragment() {
+
+    private val TAG = "GeminiFragment"
 
     private var _binding: FragmentGeminiBinding? = null
     private val binding get() = _binding!!
@@ -27,10 +38,18 @@ class GeminiFragment : Fragment() {
     private lateinit var mMessageAdapter: MessageListAdapter
     private var messageList = listOf<Message>()
 
+    private lateinit var startDate: TextView
+
     private var messageId = 0
+    private var userEditText = ""
 
     private val gemini = User("Gemini", "Gemini")
     private val user = User("User", "User")
+
+    private val generativeModel = GenerativeModel(
+        modelName = "gemini-1.5-flash",
+        apiKey = BuildConfig.API_KEY
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -59,18 +78,65 @@ class GeminiFragment : Fragment() {
         binding.geminiChatSendImageview.setOnClickListener {
 
             if(binding.geminiChatMessageEdittext.text.toString() != "") {
-                messageList += Message(messageId.toString(), binding.geminiChatMessageEdittext.text.toString(), user, getTime())
+                hideKeyboard()
 
                 displayMessageList()
+
+                userEditText = binding.geminiChatMessageEdittext.text.toString()
+
+                binding.geminiChatMessageEdittext.setText("")
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    postMessage2Gemini(binding.geminiChatMessageEdittext.text.toString())
+
+                    withContext(Dispatchers.Main) {
+                        displayMessageList()
+                    }
+                }
             }
             else {
                 Toast.makeText(activity, "質問等を入力してください", Toast.LENGTH_SHORT).show()
             }
 
         }
+
+        binding.recyclerChat.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboard()
+            }
+            false
+        }
+    }
+
+    suspend fun postMessage2Gemini(message: String) {
+        try {
+
+            var chatHistoryMessageList = listOf<Content>()
+
+            val roleList = listOf("model", "user")
+
+            for(index in messageList.indices) {
+                chatHistoryMessageList += content(role = roleList[index % 2]) {text(messageList[index].getMessage())}
+            }
+
+            messageList += Message(messageId.toString(), userEditText, user, getTime())
+
+            displayMessageList()
+
+            val chat = generativeModel.startChat(
+                history = chatHistoryMessageList
+            )
+            val resultResponse = chat.sendMessage(userEditText)
+
+            messageList += Message(messageId.toString(), resultResponse.text!!, gemini, getTime())
+        }
+        catch (e: Exception) {
+            Log.d(TAG, "Error: $e")
+        }
     }
 
     private fun initMessage() {
+
         messageList += Message(messageId.toString(), "何かお困りごとはありますか？", gemini, getTime())
 
         displayMessageList()
@@ -94,5 +160,10 @@ class GeminiFragment : Fragment() {
         val minute = currentDateTime.minute
 
         return "$hour:$minute"
+    }
+
+    private fun hideKeyboard(){
+        val inputMethodManager = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.geminiChatMessageEdittext.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 }
